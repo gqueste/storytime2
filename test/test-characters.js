20060a90fcd7fb4e0aec0a070cfe304d7fc98fbc
gqueste <gabriel.queste@gmail.com>
2017-08-07 11:12:20 +0200

    Test DELETE /characters

--- test/test-characters.js
+++ test/test-characters.js
@@ -7,8 +7,21 @@ let should = chai.should();
 chai.use(chaiHttp);
 
 let currentLength;
+let insertedCharacterId;
+
+
+function testGetAllCharacters(res) {
+    res.should.have.status(200);
+    res.body.should.be.a('object');
+    res.body.should.have.property('status');
+    res.body.should.have.property('data');
+    res.body.data.should.have.property('characters');
+    res.body.data.characters.should.be.a('array');
+}
+
+
 //Our parent block
-describe('Books', () => {
+describe('Characters', () => {
     /*
      * Test the /GET route
      */
@@ -17,12 +30,7 @@ describe('Books', () => {
             chai.request(server)
                 .get('/api/v1/characters')
                 .end((err, res) => {
-                    res.should.have.status(200);
-                    res.body.should.be.a('object');
-                    res.body.should.have.property('status');
-                    res.body.should.have.property('data');
-                    res.body.data.should.have.property('characters');
-                    res.body.data.characters.should.be.a('array');
+                    testGetAllCharacters(res);
                     currentLength = res.body.data.characters.length;
                     done();
                 });
@@ -45,6 +53,41 @@ describe('Books', () => {
                     res.body.should.have.property('status');
                     res.body.should.have.property('character');
                     res.body.character.should.be.a('object');
+                    insertedCharacterId = res.body.character['_id'];
+                    done();
+                });
+        });
+        it('it should add a character', (done) => {
+            chai.request(server)
+                .get('/api/v1/characters')
+                .end((err, res) => {
+                    testGetAllCharacters(res);
+                    res.body.data.characters.length.should.be.eql(currentLength + 1);
+                    done();
+                });
+        });
+    });
+    describe('/DELETE characters/:id', () => {
+        it('it should DELETE a character', (done) => {
+           chai.request(server)
+               .delete(`/api/v1/characters/${insertedCharacterId}`)
+               .end((err, res) => {
+                   res.should.have.status(200);
+                   res.body.should.be.a('object');
+                   res.body.should.have.property('status');
+                   res.body.should.have.property('character');
+                   res.body.character.should.be.a('object');
+                   res.body.character.should.have.property('_id');
+                   res.body.character['_id'].should.be.eql(insertedCharacterId);
+                   done();
+               });
+        });
+        it('it should remove a character', (done) => {
+            chai.request(server)
+                .get('/api/v1/characters')
+                .end((err, res) => {
+                    testGetAllCharacters(res);
+                    res.body.data.characters.length.should.be.eql(currentLength);
                     done();
                 });
         });
