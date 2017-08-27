7707425be9def99285b32bfc1d3c53a4a9e2da8d
gqueste <gabriel.queste@gmail.com>
2017-08-27 16:27:59 +0200

    Tag & Character classes, input field

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import {Component} from '@angular/core';
+import {Tag} from './tag';
+import {Character} from './character';
 
 @Component({
     selector: 'app-root',
@@ -6,29 +8,61 @@ import {Component} from '@angular/core';
     styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    title = 'app';
-    characters = [
+    title = 'StoryTime';
+    characters: Character[] = [
         {
+            _id: '1',
             name: 'Christian',
             tags: [
                 {
+                    _id: '1',
                     title: 'Pouvoir de mort'
                 },
                 {
+                    _id: '2',
                     title: 'Famille 1'
                 }
             ]
         },
         {
-            name: 'Toto'
+            _id: '2',
+            name: 'Toto',
+            tags: []
         },
         {
+            _id: '3',
             name: 'Batman',
             tags: [
                 {
+                    _id: '1',
                     title: 'Pouvoir de mort'
                 }
             ]
         }
     ];
+    possibleTags: Tag[] = [
+        {
+            _id: '1',
+            title: 'Pouvoir de mort'
+        },
+        {
+            _id: '2',
+            title: 'Famille 1'
+        }
+    ];
+    currentTags: Tag[] = [
+        {
+            _id: '1',
+            title: 'Pouvoir de mort'
+        },
+        {
+            _id: '2',
+            title: 'Famille 1'
+        }
+    ];
+
+    onCharacterSelect(element: Character): void {
+        console.log("viewing character");
+        console.log(element);
+    }
 }
---
+++ front/src/app/character.ts
@@ -0,0 +1,7 @@
+import {Tag} from './tag';
+
+export class Character {
+    _id: string;
+    name: string;
+    tags: Tag[]
+}
