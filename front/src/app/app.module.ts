ce684ff274f03e137630b64ea07dd2529e4b3c6a
gqueste <gabriel.queste@gmail.com>
2017-10-28 16:22:11 +0200

    Markdown Editor

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import {HttpClientModule} from '@angular/common/http';
+import { MarkdownModule } from 'angular2-markdown';
 
 import { AppComponent } from './app.component';
 
@@ -13,7 +14,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     HttpClientModule,
-      FormsModule
+    FormsModule,
+    MarkdownModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
